c900054473222bf2d9f0ce313f2e5a2d149bad2b
synoticon <elmo_05@gmx.de>
2013-09-26 21:10:32 +0200

    26.09.2013 - 21:10 TJP - UML inkl. DB-Skripte pull. - Umstrukturierung des Blogs

---
+++ Datenbank/Skripte/deleteALL_TABLES_FromDB.sql
@@ -0,0 +1,27 @@
+DROP TABLE gr1.Rolle_Zu_Rechte;
+GO
+DROP TABLE gr1.Rechte;
+GO
+DROP TABLE gr1.Modul_Zu_Subrelease;
+GO
+DROP TABLE gr1.Download_History;
+GO
+DROP TABLE gr1.Lizenz_Zeitlich;
+GO
+DROP TABLE gr1.Lizenz_Versionsorientiert;
+GO
+DROP TABLE gr1.Benutzer_Zu_Lizenz;
+GO
+DROP TABLE gr1.Lizenz;
+GO
+DROP TABLE gr1.Benutzer;
+GO
+DROP TABLE gr1.Build;
+GO
+DROP TABLE gr1.Subrelease;
+GO
+DROP TABLE gr1.Release;
+GO
+DROP TABLE gr1.Programm;
+GO
+DROP TABLE gr1.Rolle;
