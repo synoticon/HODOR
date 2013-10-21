958f00e06ec06525d05220dcc342664c17009c6d
synoticon <elmo_05@gmx.de>
2013-10-21 14:56:58 +0200

    21.10.2013 - 14:57 TJP Navigationsstruktur durch Benutzer und Produktverwaltung. Bearbeitungsbutton in ListView unter Memberverwaltung.

---
+++ HODOR/HODOR/Members/Administration/Verwaltung.aspx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HODOR.src.DAO;
+using HODOR.src.Globals;
+
+namespace HODOR.Members.Administration
+{
+    public partial class ProduktVerwaltung : System.Web.UI.Page
+    {
+        protected List<String> searchContext = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public enum SearchType
+        {
+            NotSet = -1,
+            User = 0,
+            Product = 1,
+            Upload = 2
+        }
+
+        protected void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (this.tb_SearchInput != null)
+	        {
+                if (rb_UserSearch.Checked)
+                {
+                    MultiView1.ActiveViewIndex = (int)SearchType.User;
+                    getSearchContext();
+                }
+                else if (rb_ProductSearch.Checked)
+                {
+                    MultiView1.ActiveViewIndex = (int)SearchType.Product;
+                    getSearchContext();
+                }
+	        }
+        }
+
+        protected void getSearchContext()
+        {
+            if (MultiView1.ActiveViewIndex > -1)
+            {
+                SearchType mSearchType =
+                     (SearchType)MultiView1.ActiveViewIndex;
+
+                searchContext = DoSearch(tb_SearchInput.Text, mSearchType);
+            }
+        }
+
+        protected List<String> DoSearch(String searchTerm, SearchType type)
+        {
+            List<String> searchContext = null;
+
+            switch (type)
+            {
+                case SearchType.User:
+                    {
+                        List<Benutzer> allUsers = BenutzerDAO.getAllUsers();
+                        foreach (Benutzer user in allUsers)
+                        {
+                            if(user.Name.Contains(searchTerm) || user.NutzerNr.Contains(searchTerm))
+                            {
+                                if (lb_User.Items.FindByText(user.NutzerNr) == null)
+                                {
+                                    this.lb_User.Items.Add(new ListItem(user.NutzerNr));                                    
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case SearchType.Product:
+                    {
+                        List<Programm> allProducts = ProgrammDAO.getAllProgramme();
+                        foreach (Programm program in allProducts)
+                        {
+                            if(program.Name.Contains(searchTerm))
+                            {
+                                if (lb_User.Items.FindByText(program.Name) == null)
+                                {
+                                    this.lb_Product.Items.Add(new ListItem(program.Name));
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case SearchType.NotSet:
+                    break;
+            }
+            return searchContext;
+        }
+
+        protected void lb_SelectedIndexChanged_Main(object sender, EventArgs e)
+        {
+            int lizenz_count = 0;
+
+            if (MultiView1.ActiveViewIndex == 0)
+            {
+                Benutzer user = BenutzerDAO.getUserByKundenNrOrNull(this.lb_User.SelectedItem.Text);
+                if(user != null)
+                {
+                    this.l_Name.Text = user.Name;
+                    this.l_NutzerNr.Text = user.NutzerNr;
+                    this.l_Name.Text = user.Rolle.ToString();
+                    this.l_EMail.Text = user.Email;
+                    List<Lizenz> lizenzen = user.Lizenzs.ToList();
+                    foreach (Lizenz lizenz in lizenzen)
+                    {
+                        lizenz_count++;
+                    }
+                    this.l_LizenzAnzahl.Text = lizenz_count.ToString();
+                    this.l_LizenzZaehler.Visible = true;
+                    this.l_ende.Visible = true;
+                }
+            }
+            else if (MultiView1.ActiveViewIndex == 1)
+            {
+                Programm program = ProgrammDAO.getProgrammByExactNameOrNull(this.lb_Product.SelectedItem.Text);
+                List<Release> releases = ReleaseDAO.getAllMajorReleasesFor(program);
+                this.lb_Release.Visible = true; 
+                if(program != null)
+                {
+                    this.l_ProgrammName.Text = program.Name;
+                    foreach (Release release in releases)
+	                {
+                        if (lb_User.Items.FindByText(release.Releasenummer.ToString()) == null)
+                        {
+                            this.lb_Release.Items.Add(new ListItem(release.Releasenummer.ToString()));
+                        }
+	                }
+                }
+            }
+        }
+
+        protected void lb_SelectedIndexChanged_Sub(object sender, EventArgs e)
+        {
+            //Subrelease subRelease =  this.lb_Release.SelectedIndex;
+
+            //this.l_ReleaseNr.Text = release.Releasenummer.ToString();
+            //this.l_ReleaseDatum.Text = release.Releasedatum.ToString();
+            //this.l_Beschreibung.Text = release.Beschreibung;
+        }
+
+        /*
+         * Upload Funktionalität
+         */
+
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+                try
+                {
+                    string body = TextArea1.Value;
+
+                    FileUpload1.SaveAs("C:\\Uploads\\" +
+                         FileUpload1.FileName);
+                    Label1.Text = "File name: " +
+                         FileUpload1.PostedFile.FileName + "<br>" +
+                         FileUpload1.PostedFile.ContentLength + " kb<br>" +
+                         "Content type: " +
+                         FileUpload1.PostedFile.ContentType;
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text = "ERROR: " + ex.Message.ToString();
+                }
+            else
+            {
+                Label1.Text = "You have not specified a file.";
+            }
+        }
+    }
+}
---
+++ HODOR/HODOR/Members/Administration/Verwaltung.aspx.designer.cs
@@ -0,0 +1,276 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Der Code wurde von einem Tool generiert.
+//
+//     Änderungen an der Datei führen möglicherweise zu falschem Verhalten, und sie gehen verloren, wenn
+//     der Code erneut generiert wird. 
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace HODOR.Members.Administration {
+    
+    
+    public partial class ProduktVerwaltung {
+        
+        /// <summary>
+        /// l_SearchInput-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_SearchInput;
+        
+        /// <summary>
+        /// tb_SearchInput-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_SearchInput;
+        
+        /// <summary>
+        /// rb_UserSearch-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rb_UserSearch;
+        
+        /// <summary>
+        /// rb_ProductSearch-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rb_ProductSearch;
+        
+        /// <summary>
+        /// SearchInputValidator-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator SearchInputValidator;
+        
+        /// <summary>
+        /// b_Search-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button b_Search;
+        
+        /// <summary>
+        /// MultiView1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView MultiView1;
+        
+        /// <summary>
+        /// UserView-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View UserView;
+        
+        /// <summary>
+        /// lb_User-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox lb_User;
+        
+        /// <summary>
+        /// l_Name-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_Name;
+        
+        /// <summary>
+        /// l_NutzerNr-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_NutzerNr;
+        
+        /// <summary>
+        /// l_EMail-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_EMail;
+        
+        /// <summary>
+        /// l_Rolle-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_Rolle;
+        
+        /// <summary>
+        /// l_LizenzZaehler-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_LizenzZaehler;
+        
+        /// <summary>
+        /// l_LizenzAnzahl-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_LizenzAnzahl;
+        
+        /// <summary>
+        /// l_ende-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_ende;
+        
+        /// <summary>
+        /// ProductView-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View ProductView;
+        
+        /// <summary>
+        /// lb_Product-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox lb_Product;
+        
+        /// <summary>
+        /// l_ProgrammName-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_ProgrammName;
+        
+        /// <summary>
+        /// lb_Release-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox lb_Release;
+        
+        /// <summary>
+        /// l_ReleaseNr-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_ReleaseNr;
+        
+        /// <summary>
+        /// l_ReleaseDatum-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_ReleaseDatum;
+        
+        /// <summary>
+        /// l_Beschreibung-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label l_Beschreibung;
+        
+        /// <summary>
+        /// UploadView-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View UploadView;
+        
+        /// <summary>
+        /// FileUpload1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+        
+        /// <summary>
+        /// regexpName-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator regexpName;
+        
+        /// <summary>
+        /// Button2-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
+        /// Label1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// TextArea1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea TextArea1;
+    }
+}
